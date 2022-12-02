897bf6ba5f6fdcb8c114982f302e10bd438bc8ee
Jethro <jethrotiago@hotmail.com>
2022-12-01 23:17:59 -0300

    Criando paleta de cores

--- react-sass-portfolio/src/App.jsx
+++ react-sass-portfolio/src/App.jsx
@@ -1,9 +1,14 @@
+import MainContent from "./components/MainContent";
+import Sidebar from "./components/Sidebar";
+
 import "./styles/components/app.sass";
 
 function App() {
   return (
     <div id="portfolio">
       <h1>Jethro Tiago</h1>
+      <Sidebar />
+      <MainContent />
     </div>
   );
 }
