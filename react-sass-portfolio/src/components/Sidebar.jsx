b120dae4c88e2b94f89a7d091a952679a07afb66
Jethro <jethrotiago@hotmail.com>
2022-12-01 23:37:36 -0300

    Criando Sidebar aside

--- react-sass-portfolio/src/components/Sidebar.jsx
+++ react-sass-portfolio/src/components/Sidebar.jsx
@@ -1,7 +1,13 @@
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
+  return <aside id="sidebar">
+    <p>imagem</p>
+    <p className="title">Desenvolvedor Front-End</p>
+    <p>Redes Sociais</p>
+    <p>Informações de contato</p>
+    <a href="" className="btn">
+      Download Currículo
+    </a>
+  </aside>
 }
 
 export default Sidebar
