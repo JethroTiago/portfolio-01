fff2eef3e328c5817159515d86ba595ed2929f1b
Jethro <jethrotiago@hotmail.com>
2022-12-01 22:50:18 -0300

    Adicionando components

--- react-sass-portfolio/src/App.jsx
+++ react-sass-portfolio/src/App.jsx
@@ -1,7 +1,9 @@
+import "./styles/components/app.sass";
+
 function App() {
   return (
-    <div className="App">
-      <h1>React - Sass - Portfolio</h1>
+    <div id="portfolio">
+      <h1>Jethro Tiago</h1>
     </div>
   );
 }
