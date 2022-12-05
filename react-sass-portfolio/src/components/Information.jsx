938c6069d29700eb96b4bd286d975301db3e2504
Jethro <jethrotiago@hotmail.com>
2022-12-05 20:20:18 -0300

    Adicionando dados a area de informações

--- react-sass-portfolio/src/components/Information.jsx
+++ react-sass-portfolio/src/components/Information.jsx
@@ -8,8 +8,22 @@ const Information = () => {
         <AiFillPhone id="phone-icon" />
         <div>
             <h3>Telefone</h3>
+            <p>(71) 99296-9091</p>
         </div>
-
+    </div>
+    <div className="info-card">
+        <AiOutlineMail id="mail-icon" />
+        <div>
+            <h3>E-mail</h3>
+            <p>jethrotiago@hotmail.com</p>
+        </div>
+    </div>
+    <div className="info-card">
+        <AiFillEnvironment id="pin-icon" />
+        <div>
+            <h3>Localização</h3>
+            <p>Curitiba / Paraná</p>
+        </div>
     </div>
 
   </section>
