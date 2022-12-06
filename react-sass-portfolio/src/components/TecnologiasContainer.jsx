4330694a4abc0925834192d47d92eb093ce6c258
Jethro <jethrotiago@hotmail.com>
2022-12-06 20:37:18 -0300

    Criando tecnologiascontainer

--- react-sass-portfolio/src/components/TecnologiasContainer.jsx
+++ react-sass-portfolio/src/components/TecnologiasContainer.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import {
+  DiHtml5,
+  DiCss3,
+  DiJsBadge,
+  DiReact,
+  DiSass,
+} from 'react-icons/di';
+
+import "../styles/components/tecnologiascontainer.sass";
+
+const tecnologias = [
+  { id: "html", name: "HTML5", icon: <DiHtml5 /> },
+  { id: "css", name: "CSS3", icon: <DiCss3 /> },
+  { id: "js", name: "JavaScript", icon: <DiJsBadge /> },
+  { id: "react", name: "React", icon: <DiReact /> },
+  { id: "sass", name: "Sass", icon: <DiSass /> },
+];
 
 const TecnologiasContainer = () => {
-  return <div>TecnologiasContainer</div>;
+  return <section className='tecnologias-container'>
+    <h2>Tecnologias</h2>
+    <div className="tecnologias-grid">
+      {tecnologias.map((tech) =>(
+        <div className="tecnologia-card" id={tech.id} key={tech.id}>
+          {tech.icon}
+          <div className="tecnologia-info">
+            <h3>{tech.name}</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+          </div>
+        </div>
+      ))}
+    </div>
+
+  </section>
   
 };
 
